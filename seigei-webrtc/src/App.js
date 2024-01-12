f163b27eabe5dbb4416c0b9af5c7c0742e60d5f7
ROHITH S <rohith.dev@zohomail.in>
2024-01-13 00:34:37 +0530

    Co-authored-by: Rishi-kumar18 <Rishi-kumar18@users.noreply.github.com>

---
+++ seigei-webrtc/src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css';
+import { VideoRoom } from './Components/VideoRoom';
+
+function App() {
+  const [joined, setJoined] = useState(false);
+
+  return (
+    <div className="App">
+      <h1>WDJ Virtual Call</h1>
+
+      {!joined && (
+        <button onClick={() => setJoined(true)}>
+          Join Room
+        </button>
+      )}
+
+      {joined && (
+        <>
+          <button onClick={() => setJoined(false)}>
+            To Lobby
+          </button>
+          <VideoRoom />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
